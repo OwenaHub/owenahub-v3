1d3db368ddc0627486f0c67f3e350ee8eb105a27
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2025-05-09 08:52:59 +0100

    chore: update react-router to version 7.5.3

--- app/lib/keys.ts
+++ app/lib/keys.ts
@@ -1,8 +1,8 @@
 export const storageKeys = {
-    user: "@owanhub:user",
-    route: "@owanhub:intended_route",
+    user: "@owenahub:user",
+    route: "@owenahub:intended_route",
 };
 
 export const BASE_URL = import.meta.env.BASE_URL;
 export const API_URL = import.meta.env.VITE_API_URL;
-export const STORAGE_URL = import.meta.env.VITE_STORAGE_URL
+export const STORAGE_URL = import.meta.env.VITE_STORAGE_URL;
